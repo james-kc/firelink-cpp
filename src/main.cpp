5fb718f3c0bf5395ddad2911fd9266c2eb5ce0b5
JamesK-C <j.kavanaghcranston@gmail.com>
2025-10-05 18:44:00 +0100

    Now trying IMU

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,38 @@
 int main() {
 
     // // BME280 Test
-    BME280 bme;
-    bme.begin();
-    float pad_pressure = bme.calibrateAltitude();
-
-    while (true) {
-        float temp = bme.readTemperature();
-        float pressure = bme.readPressure();
-        float altitude = bme.readAltitude(pad_pressure);
-        float humidity = bme.readHumidity();
-        std::cout << "Temp: " << temp << " °C, "
-                  << "Pressure: " << pressure << " hPa, "
-                  << "Altitude: " << altitude << " m, "
-                  << "Humidity: " << humidity << " %" << std::endl;
-        sleep(1);
-    }
-
-    // IMU Test
-    // IMU imu;
-    // if (!imu.begin()) { std::cerr << "Failed to initialize IMU" << std::endl; return 1; }
-
-    // std::cout << "IMU initialized. ID: 0x" << std::hex << (int)imu.getChipID() << std::dec << std::endl;
+    // BME280 bme;
+    // bme.begin();
+    // float pad_pressure = bme.calibrateAltitude();
 
     // while (true) {
-    //     float ax, ay, az, gx, gy, gz;
-    //     imu.readAccel(ax, ay, az);
-    //     imu.readGyro(gx, gy, gz);
-
-    //     std::cout << "Accel [m/s²]: X=" << ax << " Y=" << ay << " Z=" << az
-    //               << " | Gyro [°/s]: X=" << gx << " Y=" << gy << " Z=" << gz << std::endl;
-
+    //     float temp = bme.readTemperature();
+    //     float pressure = bme.readPressure();
+    //     float altitude = bme.readAltitude(pad_pressure);
+    //     float humidity = bme.readHumidity();
+    //     std::cout << "Temp: " << temp << " °C, "
+    //               << "Pressure: " << pressure << " hPa, "
+    //               << "Altitude: " << altitude << " m, "
+    //               << "Humidity: " << humidity << " %" << std::endl;
     //     sleep(1);
     // }
+
+    IMU Test
+    IMU imu;
+    if (!imu.begin()) { std::cerr << "Failed to initialize IMU" << std::endl; return 1; }
+
+    std::cout << "IMU initialized. ID: 0x" << std::hex << (int)imu.getChipID() << std::dec << std::endl;
+
+    while (true) {
+        float ax, ay, az, gx, gy, gz;
+        imu.readAccel(ax, ay, az);
+        imu.readGyro(gx, gy, gz);
+
+        std::cout << "Accel [m/s²]: X=" << ax << " Y=" << ay << " Z=" << az
+                  << " | Gyro [°/s]: X=" << gx << " Y=" << gy << " Z=" << gz << std::endl;
+
+        sleep(1);
+    }
 
     return 0;
 }
