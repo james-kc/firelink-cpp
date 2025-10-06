aa6bf3717829562e68591ff3f661bef718efca85
JamesK-C <j.kavanaghcranston@gmail.com>
2025-10-06 18:33:37 +0100

    Another change to plotting accel

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,15 @@ void printGyroBars(float gx, float gy, float gz) {
     std::cout.flush();
 }
 
+void printoTrace(float gx) {
+    int mid = 40;
+    int pos = std::clamp(mid + (int)(gx * 10), 0, 79);
+
+    for (int i = 0; i < 80; ++i)
+        std::cout << (i == pos ? '*' : ' ');
+    std::cout << '\n';
+}
+
 int main() {
 
     // BME280 Test
@@ -60,13 +69,8 @@ int main() {
 
         // std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        std::cout << "\r"; // return to start of line
-        std::cout << "X [" << gx << "] "
-                << "Y [" << gy << "] "
-                << "Z [" << gz << "]   ";
-        std::cout.flush();
-
         // printGyroBars(gx, gy, gz);
+        printGyroTrace(az);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
