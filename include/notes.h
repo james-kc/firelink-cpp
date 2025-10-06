51eba1451e819e3f4b018104ee6dc92a150f5f09
james-kc <j.kavanaghcranston@gmail.com>
2025-10-06 15:38:33 +0100

    Updated to use common naming of Eb

--- include/notes.h
+++ include/notes.h
@@ -7,7 +7,7 @@ namespace Notes {
     constexpr int C2  = 65;
     constexpr int Cs2 = 69;  // C#2 / Db2
     constexpr int D2  = 73;
-    constexpr int Ds2 = 78;
+    constexpr int Eb2 = 78;
     constexpr int E2  = 82;
     constexpr int F2  = 87;
     constexpr int Fs2 = 93;
@@ -20,7 +20,7 @@ namespace Notes {
     constexpr int C3  = 131;
     constexpr int Cs3 = 139;
     constexpr int D3  = 147;
-    constexpr int Ds3 = 156;
+    constexpr int Eb3 = 156;
     constexpr int E3  = 165;
     constexpr int F3  = 175;
     constexpr int Fs3 = 185;
@@ -33,7 +33,7 @@ namespace Notes {
     constexpr int C4  = 262;
     constexpr int Cs4 = 277;
     constexpr int D4  = 294;
-    constexpr int Ds4 = 311;
+    constexpr int Eb4 = 311;
     constexpr int E4  = 330;
     constexpr int F4  = 349;
     constexpr int Fs4 = 370;
@@ -46,7 +46,7 @@ namespace Notes {
     constexpr int C5  = 523;
     constexpr int Cs5 = 554;
     constexpr int D5  = 587;
-    constexpr int Ds5 = 622;
+    constexpr int Eb5 = 622;
     constexpr int E5  = 659;
     constexpr int F5  = 698;
     constexpr int Fs5 = 740;
@@ -59,7 +59,7 @@ namespace Notes {
     constexpr int C6  = 1047;
     constexpr int Cs6 = 1109;
     constexpr int D6  = 1175;
-    constexpr int Ds6 = 1245;
+    constexpr int Eb6 = 1245;
     constexpr int E6  = 1319;
     constexpr int F6  = 1397;
     constexpr int Fs6 = 1480;
