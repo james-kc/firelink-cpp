5fa10ec6521b3cd692f2ccae7e52847323444c96
james-kc <j.kavanaghcranston@gmail.com>
2025-10-08 17:44:24 +0100

    Loading BME test

--- src/main.cpp
+++ src/main.cpp
@@ -36,44 +36,44 @@ void printTrace(float gx) {
 int main() {
 
     // BME280 Test
-    // BME280 bme;
-    // bme.begin();
-    // float pad_pressure = bme.calibrateAltitude();
-
-    // while (true) {
-    //     float temp = bme.readTemperature();
-    //     float pressure = bme.readPressure();
-    //     float altitude = bme.readAltitude(pad_pressure);
-    //     float humidity = bme.readHumidity();
-    //     std::cout << "Temp: " << temp << " °C, "
-    //               << "Pressure: " << pressure << " hPa, "
-    //               << "Altitude: " << altitude << " m, "
-    //               << "Humidity: " << humidity << " %" << std::endl;
-    //     std::this_thread::sleep_for(std::chrono::seconds(1));
-    // }
-
-    // IMU Test
-    IMU imu;
-    if (!imu.begin()) { std::cerr << "Failed to initialize IMU" << std::endl; return 1; }
-
-    std::cout << "IMU initialized. ID: 0x" << std::hex << (int)imu.getChipID() << std::dec << std::endl;
+    BME280 bme;
+    bme.begin();
+    float pad_pressure = bme.calibrateAltitude();
 
     while (true) {
-        float ax, ay, az, gx, gy, gz;
-        imu.readAccel(ax, ay, az);
-        imu.readGyro(gx, gy, gz);
-
-        // std::cout << "Accel [m/s²]: X=" << ax << " Y=" << ay << " Z=" << az
-        //           << " | Gyro [°/s]: X=" << gx << " Y=" << gy << " Z=" << gz << std::endl;
-
-        // std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        // printGyroBars(gx, gy, gz);
-        printTrace(az);
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        float temp = bme.readTemperature();
+        float pressure = bme.readPressure();
+        float altitude = bme.readAltitude(pad_pressure);
+        float humidity = bme.readHumidity();
+        std::cout << "Temp: " << temp << " °C, "
+                  << "Pressure: " << pressure << " hPa, "
+                  << "Altitude: " << altitude << " m, "
+                  << "Humidity: " << humidity << " %" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    return 0;
+    // IMU Test
+    // IMU imu;
+    // if (!imu.begin()) { std::cerr << "Failed to initialize IMU" << std::endl; return 1; }
+
+    // std::cout << "IMU initialized. ID: 0x" << std::hex << (int)imu.getChipID() << std::dec << std::endl;
+
+    // while (true) {
+    //     float ax, ay, az, gx, gy, gz;
+    //     imu.readAccel(ax, ay, az);
+    //     imu.readGyro(gx, gy, gz);
+
+    //     // std::cout << "Accel [m/s²]: X=" << ax << " Y=" << ay << " Z=" << az
+    //     //           << " | Gyro [°/s]: X=" << gx << " Y=" << gy << " Z=" << gz << std::endl;
+
+    //     // std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    //     // printGyroBars(gx, gy, gz);
+    //     printTrace(az);
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    // }
+
+    // return 0;
 
     // GPS Test
     // GPS gps;
