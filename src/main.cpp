7d1819bc1cc7122a0e03586ddb6722253304852a
james-kc <j.kavanaghcranston@gmail.com>
2025-10-09 01:33:55 +0100

    Check for data at a faster rate

--- src/main.cpp
+++ src/main.cpp
@@ -89,10 +89,7 @@ int main() {
                       << " | Alt: " << d.altitude_m << " m"
                       << std::endl;
         }
-        else {
-            std::cout << "No GPS fix..." << std::endl;
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     // // Buzzer Test
