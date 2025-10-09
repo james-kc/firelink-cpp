95df6d78448a3a4d1fced8c2d9e776f600f53920
james-kc <j.kavanaghcranston@gmail.com>
2025-10-09 01:43:16 +0100

    poll for new data at 1Hz

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main() {
                     << std::endl;
 
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     // // Buzzer Test
